b36568d6aa8af2d2fa88db7896e62da83d578148
nickguerra <nickg@maliciousgames.com>
2023-10-14 05:53:37 -0500

    updated notes

--- src/array-test.ts
+++ src/array-test.ts
@@ -1,3 +1,5 @@
+// demo of JS [] syntax
+// not an array, but array list
 
 const a: number[] = [];
 
@@ -32,45 +34,45 @@ function pop(number: number) {
 }
 
 function get(idx: number) {
-    return function() {
+    return function () {
         return a[idx];
     };
 }
 
 function push_arr(count: number) {
-    return function() {
+    return function () {
         push(count);
     };
 }
 
 function pop_arr(count: number) {
-    return function() {
+    return function () {
         pop(count);
     };
 }
 
 function unshift_arr(count: number) {
-    return function() {
+    return function () {
         unshift(count);
     };
 }
 
 function shift_arr(count: number) {
-    return function() {
+    return function () {
         shift(count);
     };
 }
 
 const tests = [10, 100, 1000, 10000, 100000, 1_000_000, 10_000_000];
 console.log("Testing get");
-tests.forEach(t => {
+tests.forEach((t) => {
     a.length = 0;
     push(t);
     console.log(t, time(get(t - 1)));
 });
 
 console.log("push");
-tests.forEach(t => {
+tests.forEach((t) => {
     a.length = 0;
     push(t);
 
@@ -78,7 +80,7 @@ tests.forEach(t => {
 });
 
 console.log("pop");
-tests.forEach(t => {
+tests.forEach((t) => {
     a.length = 0;
     push(t);
 
@@ -86,7 +88,7 @@ tests.forEach(t => {
 });
 
 console.log("unshift");
-tests.forEach(t => {
+tests.forEach((t) => {
     a.length = 0;
     push(t);
 
@@ -94,7 +96,7 @@ tests.forEach(t => {
 });
 
 console.log("shift");
-tests.forEach(t => {
+tests.forEach((t) => {
     a.length = 0;
     push(t);
 
--- src/day1/ArrayList.ts
+++ src/day1/ArrayList.ts
@@ -1,27 +1,17 @@
 export default class ArrayList<T> {
+    // array list are great for push / pop 0(1)
+    // array list are not good for unshift/shift or enqueue / dequeue 0(n)
+    // enqueue vs push (enqueue inserts at the beginning and needs to walk the entire list after)
+    //                 (push inserts at the end so O(1) at the end, doesn't need to walk the list)
+
     public length: number;
 
-    
+    constructor() {}
 
-    constructor() {
-    }
-
-    prepend(item: T): void {
-
-}
-    insertAt(item: T, idx: number): void {
-
-}
-    append(item: T): void {
-
-}
-    remove(item: T): T | undefined {
-
-}
-    get(idx: number): T | undefined {
-
-}
-    removeAt(idx: number): T | undefined {
-
-}
+    prepend(item: T): void {}
+    insertAt(item: T, idx: number): void {}
+    append(item: T): void {}
+    remove(item: T): T | undefined {}
+    get(idx: number): T | undefined {}
+    removeAt(idx: number): T | undefined {}
 }
