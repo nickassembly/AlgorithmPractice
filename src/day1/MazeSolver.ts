80b2974f64de2518c8905811651e65fe7026febc
nickguerra <nickg@maliciousgames.com>
2023-10-14 06:26:27 -0500

    started base case for recursion implementation

--- src/day1/MazeSolver.ts
+++ src/day1/MazeSolver.ts
@@ -1,3 +1,44 @@
-export default function solve(maze: string[], wall: string, start: Point, end: Point): Point[] {
+function walk(
+    maze: string[],
+    wall: string,
+    curr: Point,
+    end: Point,
+    seen: boolean[][],
+): boolean {
+    // 1. Base Case
+    // off the map
+    if (
+        curr.x < 0 ||
+        curr.x >= maze[0].length ||
+        curr.y < 0 ||
+        curr.y >= maze.length
+    ) {
+        return false;
+    }
 
+    // on a wall
+    if (maze[curr.y][curr.x] === wall) {
+        return false;
+    }
+
+    // at the end
+    if (curr.x === end.x && curr.y === end.y) {
+        return true;
+    }
+
+    // have we seen it?
+    if (seen[curr.y][curr.x]) {
+        return false;
+    }
+
+    // TODO: find recursive case
+}
+
+export default function solve(
+    maze: string[],
+    wall: string,
+    start: Point,
+    end: Point,
+): Point[] {
+    // todo implement
 }
